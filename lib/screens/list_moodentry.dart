f47f641381b0afa9269b45d44a31e7904e6d68d9
isaacboentoro <isaac.jesse@ui.ac.id>
2024-11-13 12:09:37 +0700

    finish tutor 8

--- lib/screens/list_moodentry.dart
+++ lib/screens/list_moodentry.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:mental_health_tracker/widgets/left_drawer.dart';
 import 'package:mental_health_tracker/models/mood_entry.dart';
+import 'package:mental_health_tracker/widgets/left_drawer.dart';
 import 'package:pbp_django_auth/pbp_django_auth.dart';
 import 'package:provider/provider.dart';
 
@@ -8,13 +8,18 @@ class MoodEntryPage extends StatefulWidget {
   const MoodEntryPage({super.key});
 
   @override
-  _MoodEntryPageState createState() => _MoodEntryPageState();
+  State<MoodEntryPage> createState() => _MoodEntryPageState();
 }
 
 class _MoodEntryPageState extends State<MoodEntryPage> {
   Future<List<MoodEntry>> fetchMood(CookieRequest request) async {
+    // Don't forget to add the trailing slash (/) at the end of the URL!
     final response = await request.get('http://localhost:8000/json/');
+
+    // Decoding the response into JSON
     var data = response;
+
+    // Convert json data to a MoodEntry object
     List<MoodEntry> listMood = [];
     for (var d in data) {
       if (d != null) {
@@ -35,51 +40,62 @@ class _MoodEntryPageState extends State<MoodEntryPage> {
       body: FutureBuilder(
         future: fetchMood(request),
         builder: (context, AsyncSnapshot snapshot) {
-          if (snapshot.data ==  null) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
             return const Center(child: CircularProgressIndicator());
-          } else {
-            if (!snapshot.hasData) {
-              return const Column(
-                children: [
-                  Text(
-                    'There is no mood data in mental health tracker.',
-                    style: TextStyle(fontSize: 20, color: Color(0xff59A5D8)),
-                  ),
-                  SizedBox(height: 8),
-                ],
-              );
-            } else {
-              return ListView.builder(
-                itemCount: snapshot.data!.length,
-                itemBuilder: (_, index) => Container(
-                  margin:
-                      const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
-                  padding: const EdgeInsets.all(20.0),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        "${snapshot.data![index].fields.mood}",
-                        style: const TextStyle(
-                          fontSize: 18.0,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      const SizedBox(height: 10),
-                      Text("${snapshot.data![index].fields.feelings}"),
-                      const SizedBox(height: 10),
-                      Text("${snapshot.data![index].fields.moodIntensity}"),
-                      const SizedBox(height: 10),
-                      Text("${snapshot.data![index].fields.time}")
-                    ],
-                  ),
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          } else if (!snapshot.hasData || snapshot.data.isEmpty) {
+            return const Column(
+              children: [
+                Text(
+                  'There is no mood data in mental health tracker.',
+                  style: TextStyle(fontSize: 20, color: Color(0xff59A5D8)),
                 ),
-              );
-            }
+                SizedBox(height: 8),
+              ],
+            );
+          } else {
+            return ListView.builder(
+              itemCount: snapshot.data.length,
+              itemBuilder: (_, index) => Container(
+                margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+                padding: const EdgeInsets.all(20.0),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(8.0),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.5),
+                      spreadRadius: 2,
+                      blurRadius: 5,
+                      offset: const Offset(0, 3),
+                    ),
+                  ],
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "${snapshot.data[index].fields.mood}",
+                      style: const TextStyle(
+                        fontSize: 18.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    Text("${snapshot.data[index].fields.feelings}"),
+                    const SizedBox(height: 10),
+                    Text("${snapshot.data[index].fields.moodIntensity}"),
+                    const SizedBox(height: 10),
+                    Text("${snapshot.data[index].fields.time}")
+                  ],
+                ),
+              ),
+            );
           }
-        }
-      )
+        },
+      ),
     );
   }
 }
--- lib/screens/moodentry_form.dart
+++ lib/screens/moodentry_form.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
-import '../widgets/left_drawer.dart';
+import 'package:mental_health_tracker/screens/menu.dart';
+import 'package:mental_health_tracker/widgets/left_drawer.dart';
+import 'package:pbp_django_auth/pbp_django_auth.dart';
+import 'package:provider/provider.dart';
+import 'dart:convert';
 
 class MoodEntryFormPage extends StatefulWidget {
   const MoodEntryFormPage({super.key});
@@ -10,153 +14,156 @@ class MoodEntryFormPage extends StatefulWidget {
 
 class _MoodEntryFormPageState extends State<MoodEntryFormPage> {
   final _formKey = GlobalKey<FormState>();
-  String _mood = "";
-  String _feelings = "";
+  String _mood = '';
+  String _feelings = '';
   int _moodIntensity = 0;
+
   @override
   Widget build(BuildContext context) {
+    final request = context.watch<CookieRequest>();
+
     return Scaffold(
-  appBar: AppBar(
-    title: const Center(
-      child: Text(
-        'Add Your Mood Today',
-      ),
-    ),
-    backgroundColor: Theme.of(context).colorScheme.primary,
-    foregroundColor: Colors.white,
-  ),
-  drawer: const LeftDrawer(),
-  body: Form(
-    key: _formKey,
-    child: SingleChildScrollView(
-      child: Column(
-  crossAxisAlignment: CrossAxisAlignment.start,
-  children: [
-    Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: TextFormField(
-        decoration: InputDecoration(
-          hintText: "Mood",
-          labelText: "Mood",
-          border: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(5.0),
+      appBar: AppBar(
+        title: const Center(
+          child: Text(
+            'Add Your Mood Today',
           ),
         ),
-        onChanged: (String? value) {
-          setState(() {
-            _mood = value!;
-          });
-        },
-        validator: (String? value) {
-          if (value == null || value.isEmpty) {
-            return "Mood cannot be empty!";
-          }
-          return null;
-        },
+        backgroundColor: Theme.of(context).colorScheme.primary,
+        foregroundColor: Colors.white,
       ),
-    ),
-    Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: TextFormField(
-        decoration: InputDecoration(
-          hintText: "Feelings",
-          labelText: "Feelings",
-          border: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(5.0),
-          ),
-        ),
-        onChanged: (String? value) {
-          setState(() {
-            _feelings = value!;
-          });
-        },
-        validator: (String? value) {
-          if (value == null || value.isEmpty) {
-            return "Feelings cannot be empty!";
-          }
-          return null;
-        },
-      ),
-    ),
-    Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: TextFormField(
-        decoration: InputDecoration(
-          hintText: "Mood intensity",
-          labelText: "Mood intensity",
-          border: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(5.0),
-          ),
-        ),
-        onChanged: (String? value) {
-          setState(() {
-            _moodIntensity = int.tryParse(value!) ?? 0;
-          });
-        },
-        validator: (String? value) {
-          if (value == null || value.isEmpty) {
-            return "Mood intensity cannot be empty!";
-          }
-          if (int.tryParse(value) == null) {
-            return "Mood intensity must be a number!";
-          }
-          return null;
-        },
-      ),
-    ),
-    Align(
-      alignment: Alignment.bottomCenter,
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: ElevatedButton(
-          style: ButtonStyle(
-            backgroundColor: WidgetStateProperty.all(
-                Theme.of(context).colorScheme.primary),
-          ),
-          onPressed: () {
-            if (_formKey.currentState!.validate()) {
-                  if (_formKey.currentState!.validate()) {
-      showDialog(
-        context: context,
-        builder: (context) {
-          return AlertDialog(
-            title: const Text('Mood successfully saved'),
-            content: SingleChildScrollView(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text('Mood: $_mood'),
-                  Text('Feelings: $_feelings'),
-                  Text('Intensity: $_moodIntensity')
-                ],
+      drawer: const LeftDrawer(),
+      body: Form(
+        key: _formKey,
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    hintText: "Mood",
+                    labelText: "Mood",
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(5.0),
+                    ),
+                  ),
+                  onChanged: (String? value) {
+                    setState(() {
+                      _mood = value!;
+                    });
+                  },
+                  validator: (String? value) {
+                    if (value == null || value.isEmpty) {
+                      return "Mood tidak boleh kosong!";
+                    }
+                    return null;
+                  },
+                ),
               ),
-            ),
-            actions: [
-              TextButton(
-                child: const Text('OK'),
-                onPressed: () {
-                  Navigator.pop(context);
-                  _formKey.currentState!.reset();
-                },
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    hintText: "Feelings",
+                    labelText: "Feelings",
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(5.0),
+                    ),
+                  ),
+                  onChanged: (String? value) {
+                    setState(() {
+                      _feelings = value!;
+                    });
+                  },
+                  validator: (String? value) {
+                    if (value == null || value.isEmpty) {
+                      return "Feelings tidak boleh kosong!";
+                    }
+                    return null;
+                  },
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    hintText: "Mood intensity",
+                    labelText: "Mood intensity",
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(5.0),
+                    ),
+                  ),
+                  onChanged: (String? value) {
+                    setState(() {
+                      _moodIntensity = int.tryParse(value!) ?? 0;
+                    });
+                  },
+                  validator: (String? value) {
+                    if (value == null || value.isEmpty) {
+                      return "Mood intensity tidak boleh kosong!";
+                    }
+                    if (int.tryParse(value) == null) {
+                      return "Mood intensity harus berupa angka!";
+                    }
+                    return null;
+                  },
+                ),
+              ),
+              Align(
+                alignment: Alignment.bottomCenter,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ElevatedButton(
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(
+                          Theme.of(context).colorScheme.primary),
+                    ),
+                    onPressed: () async {
+                        if (_formKey.currentState!.validate()) {
+                            // Send request to Django and wait for the response
+                            // TODO: Change the URL to your Django app's URL. Don't forget to add the trailing slash (/) if needed.
+                            final response = await request.postJson(
+                                "http://localhost:8000/create-flutter/",
+                                jsonEncode(<String, String>{
+                                    'mood': _mood,
+                                    'mood_intensity': _moodIntensity.toString(),
+                                    'feelings': _feelings,
+                                // TODO: Adjust the fields with your project
+                                }),
+                            );
+                            if (context.mounted) {
+                                if (response['status'] == 'success') {
+                                    ScaffoldMessenger.of(context)
+                                        .showSnackBar(const SnackBar(
+                                    content: Text("New mood has saved successfully!"),
+                                    ));
+                                    Navigator.pushReplacement(
+                                        context,
+                                        MaterialPageRoute(builder: (context) => MyHomePage()),
+                                    );
+                                } else {
+                                    ScaffoldMessenger.of(context)
+                                        .showSnackBar(const SnackBar(
+                                        content:
+                                            Text("Something went wrong, please try again."),
+                                    ));
+                                }
+                            }
+                        }
+                    },
+                    child: const Text(
+                      "Save",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ),
               ),
             ],
-          );
-        },
-      );
-    }
-            }
-          },
-          child: const Text(
-            "Save",
-            style: TextStyle(color: Colors.white),
           ),
         ),
       ),
-    ),
-  ],
-),
-    ),
-  ),
-);
+    );
   }
 }
